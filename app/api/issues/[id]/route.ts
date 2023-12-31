85284bdb846673544d43c494cbbbeae60e56e343
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-12-31 18:29:00 -0500

    built delete api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -28,3 +28,21 @@ export async function PATCH(request: NextRequest,
     });
     return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(params.id)
+        }
+    });
+    if(!issue)
+        return NextResponse.json({ error: 'Issue not found.' }, { status: 404 });
+    await prisma.issue.delete({
+        where: {
+            id: issue.id
+        }
+    });
+    return NextResponse.json({});
+}
--- app/issues/[id]/IssueDeleteBtn.tsx
+++ app/issues/[id]/IssueDeleteBtn.tsx
@@ -1,32 +1,30 @@
 'use client';
 
-import { Box, Button, AlertDialog, Flex } from '@radix-ui/themes';
+import { Button, AlertDialog, Flex } from '@radix-ui/themes';
 
 const IssueDeleteBtn = ({ issueID }: { issueID: number }) => {
   return (
-    <Box>
-        <AlertDialog.Root>
-            <AlertDialog.Trigger>
-            <Button color='red' className='lg:w-40'>Delete</Button>
-            </AlertDialog.Trigger>
-            <AlertDialog.Content>
-                <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-                <AlertDialog.Description>Do you want to permanently delete this issue?</AlertDialog.Description>
-                <Flex gap='4' mt='4'>
-                    <AlertDialog.Cancel>
-                        <Button variant='soft'>
-                            Cancel
-                        </Button>
-                    </AlertDialog.Cancel>
-                    <AlertDialog.Action>
-                        <Button variant='solid' color='red'>
-                            Delete
-                        </Button>
-                    </AlertDialog.Action>
-                </Flex>
-            </AlertDialog.Content>
-        </AlertDialog.Root>
-    </Box>
+    <AlertDialog.Root>
+        <AlertDialog.Trigger>
+        <Button color='red' className='lg:w-40'>Delete</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+            <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+            <AlertDialog.Description>Do you want to permanently delete this issue?</AlertDialog.Description>
+            <Flex gap='4' mt='4'>
+                <AlertDialog.Cancel>
+                    <Button variant='soft'>
+                        Cancel
+                    </Button>
+                </AlertDialog.Cancel>
+                <AlertDialog.Action>
+                    <Button variant='solid' color='red'>
+                        Delete
+                    </Button>
+                </AlertDialog.Action>
+            </Flex>
+        </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
