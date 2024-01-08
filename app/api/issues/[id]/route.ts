262fe30dcb5c513c2638855d23bf2c431ba399aa
Veronika Pomyateeva <pomyateevav@gmail.com>
2024-01-07 21:59:34 -0500

    added middleware to secure routes and api endpoints as well as app components from unauthorized users who are not logged in

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,16 @@
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import authOptions from "../../auth/authOptions";
 
 export async function PATCH(request: NextRequest, 
     { params }: { params: { id: string } }
 ) {
+    const session = await getServerSession(authOptions);
+    if(!session)
+        return NextResponse.json({},{ status: 401 }); // unauthorized
+
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
     if(!validation.success)
@@ -32,6 +38,11 @@ export async function PATCH(request: NextRequest,
 export async function DELETE(request: NextRequest,
     { params }: { params: { id: string } }
 ) {
+
+    const session = await getServerSession(authOptions);
+    if(!session)
+        return NextResponse.json({},{ status: 401 }); // unauthorized
+
     const issue = await prisma.issue.findUnique({
         where: {
             id: parseInt(params.id)
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,9 +4,13 @@ import { notFound } from 'next/navigation';
 import IssueDeleteBtn from './IssueDeleteBtn';
 import IssueDetails from './IssueDetails';
 import IssueEditBtn from './IssueEditBtn';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/api/auth/authOptions';
 // Update status Btn and API
 
 const IssueDetailsPage = async ({ params } : { params: { id: string } }) => {
+
+    const session = await getServerSession(authOptions);
 
     if(isNaN(parseInt(params.id))) notFound(); // if text, not number entered
 
@@ -23,13 +27,12 @@ const IssueDetailsPage = async ({ params } : { params: { id: string } }) => {
                 <Box className='space-y-4 md:col-span-4 mr-6'>
                     <IssueDetails issue={issue} />
                 </Box>
-                <Box mt='4'>
+                {session && <Box mt='4'>
                     <Flex direction={{ md: 'column' }} gap="4" align='center'>
                         <IssueEditBtn issueID={issue.id} /> 
                         <IssueDeleteBtn issueID={issue.id} /> 
                     </Flex>
-                </Box>
-                
+                </Box>}
         </Grid>
     )
 }
