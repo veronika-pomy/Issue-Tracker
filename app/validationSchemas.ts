665028b3276fca0df1db1ae79bb9b171c86054a5
Veronika Pomyateeva <pomyateevav@gmail.com>
2024-01-11 20:27:23 -0500

    added optional issima to validate patch fields for an issue

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,14 @@
+import { Status } from '@prisma/client';
 import { z } from 'zod';
 
 export const issueSchema = z.object({
-    title: z.string().min(1, { message: "Required" }).max(255),
-    description: z.string().min(1, { message: "Required" })
+    title: z.string().min(1, { message: "Issue title is required." }).max(255),
+    description: z.string().min(1, { message: "Description is required." }).max(65535),
+});
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1, { message: "Issue title is required." }).max(255).optional(),
+    description: z.string().min(1, { message: "Description is required." }).max(65535).optional(),
+    assignedUserId: z.string().min(1, "User Id is required.").max(255).optional().nullable(),
+    status: z.nativeEnum(Status).optional(),
 });
