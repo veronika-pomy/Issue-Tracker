13118752b36547a655466e148608ad4610dae085
Veronika Pomyateeva <pomyateevav@gmail.com>
2024-01-11 20:47:12 -0500

    modified api patch request to be able to optionally mod title, description or assigned user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,8 +1,10 @@
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import authOptions from "../../auth/authOptions";
+
+// TODO: add status patch request
 
 export async function PATCH(request: NextRequest, 
     { params }: { params: { id: string } }
@@ -12,26 +14,42 @@ export async function PATCH(request: NextRequest,
         return NextResponse.json({},{ status: 401 }); // unauthorized
 
     const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+
+    const validation = patchIssueSchema.safeParse(body);
+
     if(!validation.success)
         return NextResponse.json(validation.error.format(), { status: 400 }); // bad request
+
+    const { title, description, assignedUserId } = body;
+
+    if(assignedUserId) {
+        const user = await prisma.user.findUnique({
+            where: { id: assignedUserId }
+        });
+        if(!user)
+            return NextResponse.json({ error: 'Invalid user.'}, { status: 400 }); // bad request
+    };
+
     const issue = await prisma.issue.findUnique({
         where: {
             id: parseInt(params.id)
         }
     });
+
     if(!issue)
         return NextResponse.json({ error: 'Issue not found.' },{ status: 404 }); // not found
+
     const updatedIssue = await prisma.issue.update({
         where: {
             id: issue.id
         },
         data: {
-            title: body.title,
-            description: body.description
-
+            title,
+            description,
+            assignedUserId
         }
     });
+
     return NextResponse.json(updatedIssue);
 }
 
