0d85bb67579eea5924cee608864142c1b60d73dc
Veronika Pomyateeva <pomyateevav@gmail.com>
2024-01-17 20:50:48 -0500

    added summary issues component to the dashboard

---
+++ app/Summary.tsx
@@ -0,0 +1,44 @@
+import { Status } from '@prisma/client';
+import { Card, Flex, Text } from '@radix-ui/themes';
+import Link from 'next/link';
+
+interface Props {
+    open: number;
+    inProgress: number;
+    closed: number;
+}
+
+const Summary = ({ open, inProgress, closed } : Props) => {
+
+    const summary: {
+        label: string,
+        value: number,
+        status: Status
+    }[] = [
+        { label: 'Open Issues', value: open, status: 'OPEN' },
+        { label: 'In Progress Issues', value: inProgress, status: 'IN_PROGRESS' },
+        { label: 'Closed Issues', value: closed, status: 'CLOSED' }
+    ];
+
+  return (
+    <Flex gap='4'>
+        {summary.map(item => (
+            <Card key={item.label}>
+                <Flex direction='column' gap='2'>
+                    <Link
+                        href={`/issues/list?status=${item.status}`}
+                        className='text-sm font-medium'
+                    >
+                        {item.label}
+                    </Link>
+                    <Text size='4' className='font-bold'>
+                        {item.value}
+                    </Text>
+                </Flex>
+            </Card>
+        ))}
+    </Flex>
+  )
+}
+
+export default Summary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import RecentIssues from "./RecentIssues";
+import Summary from "./Summary";
+import prisma from '@/prisma/client';
 
-export default function Home() {
+export default async function Home() {
+
+const openIssues = await prisma.issue.count({ where: { status: 'OPEN' }});
+const inProgressIssues = await prisma.issue.count({ where: { status: 'IN_PROGRESS' }});
+const closedIssues = await prisma.issue.count({ where: { status: 'CLOSED' }});
+
   return (
     <div>
-      <RecentIssues />
+      <Summary open={openIssues} inProgress={inProgressIssues} closed={closedIssues} />
+      {/* <RecentIssues /> */}
     </div>
   )
 }
