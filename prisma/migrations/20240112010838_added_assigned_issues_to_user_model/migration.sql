86dc118807abb3c3c73e56042faa89a37f30731b
Veronika Pomyateeva <pomyateevav@gmail.com>
2024-01-21 10:49:22 -0500

    fixed migartions to run without foreign key constraints for planetscale deploy

--- prisma/migrations/20240112010838_added_assigned_issues_to_user_model/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Issue` ADD COLUMN `assignedUserId` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedUserId_fkey` FOREIGN KEY (`assignedUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
