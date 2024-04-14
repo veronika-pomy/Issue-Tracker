6e34c44792f47f2377edaac92e01fe2541964dc0
Veronika Pomyateeva <pomyateevav@gmail.com>
2024-04-14 12:59:54 -0400

    configured tidb prisma adapter

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,14 +1,26 @@
+import { connect } from '@tidbcloud/serverless';
+import { PrismaTiDBCloud } from '@tidbcloud/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+
+// setup
+const connectionString = `${process.env.DATABASE_URL}`;
 
 const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+ var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+// Initialize Prisma Client
+const connection = connect({ url: connectionString });
+const adapter = new PrismaTiDBCloud(connection);
+const prisma = globalThis.prisma ?? new PrismaClient({ adapter });
+
+// Planet scale
+
+// const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
 
